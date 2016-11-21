4250dab166486d086c57a13d4f6a97628ab4d062
mlabie <marc.labie@romandie.com>
2016-11-21 14:07:52 +0100

    Programme fonctionnel mais pas opti

--- Labo_06_Labie_Rouvinez.cpp
+++ Labo_06_Labie_Rouvinez.cpp
@@ -16,6 +16,10 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+
 
 //Méthode permettant de vider le Buffer
 #define VIDER_BUFFER while(cin.get() != '\n')
@@ -24,6 +28,8 @@ using namespace std;
 
 
 int saisie(int borne_inf, int borne_sup, string textSaisie);
+
+void afficheCircuit(int longueurCircuit, int amplituteVirage, int largeurRoute, int largeurTotale);
 
 bool recommencer(char OUI, char NON);
 
@@ -47,23 +53,24 @@ int main() {
    
    
    int longueurCircuit;
-   int amplituteVirage;
+   int amplitudeVirage;
    int largeurRoute;
    int largeurTotale;
+  
+   
    
    do{
       
       
-      longueurCircuit = saisie(LONG_CIRC_MIN, LONG_CIRC_MAX, "Longueur Circuit [ 0 - 1000 ]: ");
-      amplituteVirage = saisie(AMPLI_MIN, AMPLI_MAX, "Amplitue [ 1 - 3 ] : ");
-      largeurRoute    = saisie(LARG_ROUTE_MIN, LARG_ROUTE_MAX, "Largueur Route [ 3 - 10 ] : ");
-      largeurTotale   = saisie(LARG_TOTALE_MIN, LARG_TOTALE_MAX, "Largeur Totale [ 20 - 50 ] : ");
+      longueurCircuit  =  saisie(LONG_CIRC_MIN, LONG_CIRC_MAX, "Longueur Circuit [ 0 - 1000 ]: ");
+      amplitudeVirage  =  saisie(AMPLI_MIN, AMPLI_MAX, "Amplitue [ 1 - 3 ] : ");
+      largeurRoute     =  saisie(LARG_ROUTE_MIN, LARG_ROUTE_MAX, "Largueur Route [ 3 - 10 ] : ");
+      largeurTotale    =  saisie(LARG_TOTALE_MIN, LARG_TOTALE_MAX, "Largeur Totale [ 20 - 50 ] : ");
+      
+      afficheCircuit(longueurCircuit, amplitudeVirage, largeurRoute, largeurTotale);
       
       
-   }while(recommencer(OUI,NON));
-
-   
-   
+   }while(recommencer(OUI,NON)); 
    return EXIT_SUCCESS;
 }
 
@@ -95,6 +102,57 @@ int saisie(int borne_inf, int borne_sup, string textSaisie) {
    return val;
 }
 
+
+//============================      AfficheCircuit      ===========================//
+
+void afficheCircuit(int longueurCircuit, int amplitudeVirage, int largeurRoute, int largeurTotale) {
+   
+   double espace = (largeurTotale - largeurRoute)/2.0;
+   
+   int espaceGauche = floor(espace);
+   int espaceDroite = ceil(espace);
+   
+   
+   int espaceCircuit = floor(log10(longueurCircuit))+1;
+   
+   cout << espaceCircuit << endl;
+   
+   
+   int amplitude;
+   
+   string route;
+   
+   for(int i = 1; i <= largeurRoute; i++){
+      route += "=";
+   }
+   
+   bool espaceOk;
+   
+   //Iinitialisation du générateur aléatoire
+   srand((unsigned)time(NULL));
+   
+   for(int etapeCircuit = 0 ; etapeCircuit <= longueurCircuit; etapeCircuit++){
+      
+      cout << setw(0) << "" << right << setw(espaceCircuit);
+      
+      if(!(etapeCircuit%10)){
+         cout << etapeCircuit;
+      }else{
+         cout << "";
+      }
+      
+      cout << "|" << setw(espaceGauche) << "" << route << setw(espaceDroite) << "|" << endl;
+      
+      do{
+         amplitude  = rand() % (2*amplitudeVirage+1);
+         amplitude -= amplitudeVirage;
+         espaceOk   = espaceGauche > -1*amplitude && espaceDroite > amplitude;
+      }while(!espaceOk);
+      
+      espaceGauche += amplitude;
+      espaceDroite -= amplitude;
+   }
+}
 
 //============================       recommencer        ===========================//
 
