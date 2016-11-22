e686832709f11d1b28404e364138130c066041c5
mlabie <marc.labie@romandie.com>
2016-11-22 10:16:52 +0100

    Ajout de la méthode random

--- Labo_06_Labie_Rouvinez.cpp
+++ Labo_06_Labie_Rouvinez.cpp
@@ -29,7 +29,10 @@ using namespace std;
 
 int saisie(int borne_inf, int borne_sup, string textSaisie);
 
-void afficheCircuit(int longueurCircuit, int amplituteVirage, int largeurRoute, int largeurTotale);
+void afficheCircuit(int espaceGauche, int espaceDroite, string route, int etapeCircuit,
+   int espaceCircuit, int espaceMetre, int frequenceMetre);
+
+int entierAleatoire(int borne_min, int borne_max);
 
 bool recommencer(char OUI, char NON);
 
@@ -49,30 +52,74 @@ int main() {
    const int LARG_ROUTE_MAX  =  10; 
    
    const int LARG_TOTALE_MIN  =  20; 
-   const int LARG_TOTALE_MAX  =  50; 
+   const int LARG_TOTALE_MAX  =  50;
+   
+   const int FREQUENCE_METRE  =  10;
+   
+   const int ESPACE_CIRCUIT   =  6;
    
    
    int longueurCircuit;
    int amplitudeVirage;
    int largeurRoute;
    int largeurTotale;
+   
+   
+   float espaceSymetrique;          // Espace à gauche et à droite de la route
+   
+   int espaceGauche;                // Espace à gauche de la route
+   int espaceDroite;                // Espace à droite de la route
+   int espaceMetre;                 // Espacement nécessaire pour afficher les mètres
+   
+   int amplitudeActuel;
+   
+   string route;
   
+   
+   //Iinitialisation du générateur aléatoire
+   srand((unsigned)time(NULL));
+   
    
    
    do{
-      
       
       longueurCircuit  =  saisie(LONG_CIRC_MIN, LONG_CIRC_MAX, "Longueur Circuit [ 0 - 1000 ]: ");
       amplitudeVirage  =  saisie(AMPLI_MIN, AMPLI_MAX, "Amplitue [ 1 - 3 ] : ");
       largeurRoute     =  saisie(LARG_ROUTE_MIN, LARG_ROUTE_MAX, "Largueur Route [ 3 - 10 ] : ");
       largeurTotale    =  saisie(LARG_TOTALE_MIN, LARG_TOTALE_MAX, "Largeur Totale [ 20 - 50 ] : ");
       
-      afficheCircuit(longueurCircuit, amplitudeVirage, largeurRoute, largeurTotale);
+      
+      
+      espaceSymetrique  =  (largeurTotale - largeurRoute)/2.0;
+   
+      espaceGauche      =  floor(espaceSymetrique);
+      espaceDroite      =  ceil(espaceSymetrique);
+   
+      espaceMetre       =  floor(log10(longueurCircuit))+1;
+   
+      for(int segmentRoute = 1; segmentRoute <= largeurRoute; segmentRoute++){
+         route += "=";
+      }
+   
+      for(int etapeCircuit = LONG_CIRC_MIN; etapeCircuit <= longueurCircuit; etapeCircuit++){
+      
+         afficheCircuit(espaceGauche, espaceDroite, route, etapeCircuit,
+                 ESPACE_CIRCUIT, espaceMetre, FREQUENCE_METRE);
+      
+         do{
+            amplitudeActuel  = entierAleatoire(-amplitudeVirage, amplitudeVirage);
+         }while(!(espaceGauche > -amplitudeActuel && espaceDroite > amplitudeActuel));
+      
+         espaceGauche += amplitudeActuel;
+         espaceDroite -= amplitudeActuel;
+      }
       
       
    }while(recommencer(OUI,NON)); 
    return EXIT_SUCCESS;
 }
+
+
 
 
 //=================================     saisie     ================================//
@@ -103,56 +150,33 @@ int saisie(int borne_inf, int borne_sup, string textSaisie) {
 }
 
 
+
+
 //============================      AfficheCircuit      ===========================//
 
-void afficheCircuit(int longueurCircuit, int amplitudeVirage, int largeurRoute, int largeurTotale) {
+void afficheCircuit(int espaceGauche, int espaceDroite, string route, int etapeCircuit,
+   int espaceCircuit, int espaceMetre, int frequenceMetre) {
    
-   double espace = (largeurTotale - largeurRoute)/2.0;
-   
-   int espaceGauche = floor(espace);
-   int espaceDroite = ceil(espace);
-   
-   
-   int espaceCircuit = floor(log10(longueurCircuit))+1;
-   
-   cout << espaceCircuit << endl;
-   
-   
-   int amplitude;
-   
-   string route;
-   
-   for(int i = 1; i <= largeurRoute; i++){
-      route += "=";
+   cout << setw(espaceCircuit) << "" << right << setw(espaceMetre);
+      
+   if(!(etapeCircuit % frequenceMetre)){
+      cout << etapeCircuit;
    }
-   
-   bool espaceOk;
-   
-   //Iinitialisation du générateur aléatoire
-   srand((unsigned)time(NULL));
-   
-   for(int etapeCircuit = 0 ; etapeCircuit <= longueurCircuit; etapeCircuit++){
-      
-      cout << setw(0) << "" << right << setw(espaceCircuit);
-      
-      if(!(etapeCircuit%10)){
-         cout << etapeCircuit;
-      }else{
-         cout << "";
-      }
-      
-      cout << "|" << setw(espaceGauche) << "" << route << setw(espaceDroite) << "|" << endl;
-      
-      do{
-         amplitude  = rand() % (2*amplitudeVirage+1);
-         amplitude -= amplitudeVirage;
-         espaceOk   = espaceGauche > -1*amplitude && espaceDroite > amplitude;
-      }while(!espaceOk);
-      
-      espaceGauche += amplitude;
-      espaceDroite -= amplitude;
-   }
+   cout << "" << "|" << setw(espaceGauche) << "" << route << setw(espaceDroite)<< "|"
+        << endl;
 }
+
+
+
+
+//==========================       entierAleatoire        =========================//
+
+int entierAleatoire(int borne_min, int borne_max){
+   return (rand() % (borne_max - borne_min + 1)) + borne_min;
+}
+
+
+
 
 //============================       recommencer        ===========================//
 
@@ -164,7 +188,7 @@ bool recommencer(char OUI, char NON) {
    do {
 
       cout << "Voulez vous recommencer ? [" << OUI << "/" << NON << "] : ";
-      cin >> recommencer;
+      cin  >> recommencer;
       VIDER_BUFFER;
       
       recommencer = toupper(recommencer);  //met le char entré en majuscule
